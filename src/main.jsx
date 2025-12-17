ad28eb9a8780dd35683f4015bf302bc6b94217d8
Jonathan Holman <keys2xion@yahoo.com>
2025-12-17 15:29:13 -0500

    Fix: Correct file structure for Vite build

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AccelMailApp from '../AccelMail2.jsx'
+import AccelMailApp from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(
-    React.StrictMode,
-    null,
-    React.createElement(AccelMailApp)
-  ),
+  <React.StrictMode>
+    <AccelMailApp />
+  </React.StrictMode>,
 )
